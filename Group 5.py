8c73989ec9fac3abc4fc58e8389d0d25e5549151
SebasVe <159404478+SebasVe@users.noreply.github.com>
2025-02-11 16:18:52 -0800

    Add files via upload

---
+++ Group 5.py
@@ -0,0 +1,31 @@
+#Sydney Agbakoba & Sebastian Vela
+#Febuary 13, 2024
+import random
+
+def display_board(board):
+    list2d = []
+    for i in range(3):
+        list = []
+        for j in range(4):
+            list.append(0)
+        list2d.append(list)
+
+
+
+
+def reset_game():
+    #
+    
+
+def get_row():
+    #
+    user_input = input("Enter")
+
+def fire_shot(board, solution, row, col):
+    
+
+
+def main():
+    display_board()
+
+main()
